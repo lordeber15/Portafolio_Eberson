753828630dd185c8bf448a46d07be020e20dbf04
lordeber15 <eberpalomino@hotmail.com>
2023-06-12 15:28:36 -0500

    portafolio 3

--- src/components/cardsportfolio/cardporfolio.jsx
+++ src/components/cardsportfolio/cardporfolio.jsx
@@ -1,19 +1,16 @@
 import style from "./cardporfolio.module.css";
-import imagen from "../../assets/beautify.png";
-export default function cardporfolio() {
+export default function cardporfolio({ img, title, description, lenguajes }) {
   return (
     <div className={style.container}>
-      <img className={style.img} src={imagen} />
-      <div className={style.title}>Beautify</div>
-      <p className={style.description}>
-        Plataforma de E-commerce con filtros combinados, mercado pago, reserva
-        de turnos, dashboard de administrador, agregar producto, agregar
-        servicios
-      </p>
+      <img className={style.img} src={img} />
+      <div className={style.title}>{title}</div>
+      <p className={style.description}>{description}</p>
       <div className={style.containerlenguaje}>
-        <div className={style.lg}>React</div>
-        <div className={style.lg}>Redux</div>
-        <div className={style.lg}>Express</div>
+        {lenguajes.map((lg) => (
+          <div className={style.lg} key={""}>
+            {lg}
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/view/selectPortafolio/portafolio.jsx
+++ src/view/selectPortafolio/portafolio.jsx
@@ -1,11 +1,66 @@
 import style from "./portafolio.module.css";
 import Cardporfolio from "../../components/cardsportfolio/cardporfolio";
+import imagen from "../../assets/beautify.png";
+import rickamorty from "../../assets/images.jpeg";
+import dog from "../../assets/dogs.jpg";
 export default function portafolio() {
   return (
     <div className={style.container}>
       <h1 className={style.title}>Portafolio</h1>
       <div className={style.cards}>
-        <Cardporfolio />
+        <Cardporfolio
+          img={imagen}
+          title={"Beautify"}
+          description={
+            "Plataforma de E-commerce con filtros combinados, mercado pago, reserva de turnos, dashboard de administrador, agregar producto, agregar servicios"
+          }
+          lenguajes={[
+            "React",
+            "Redux",
+            "Node.js",
+            "Sequelize",
+            "Express",
+            "Html",
+            "CSS",
+            "PostgrestSQL",
+            "Firebase",
+            "Cloudinary",
+          ]}
+        />
+        <Cardporfolio
+          img={rickamorty}
+          title={"Rick an Morty"}
+          description={
+            "Pagina que consume la API de Rick and Morty con filtros combinados, busqueda por coincidencia, vista de favoritos, y un login"
+          }
+          lenguajes={[
+            "React",
+            "Redux",
+            "Node.js",
+            "Sequelize",
+            "Express",
+            "Html",
+            "CSS",
+            "PostgrestSQL",
+          ]}
+        />
+        <Cardporfolio
+          img={dog}
+          title={"API Dog's"}
+          description={
+            "Pagina que consume la API Dog's con filtros combinados, busqueda por coincidencia, vista de favoritos"
+          }
+          lenguajes={[
+            "React",
+            "Redux",
+            "Node.js",
+            "Sequelize",
+            "Express",
+            "Html",
+            "CSS",
+            "PostgrestSQL",
+          ]}
+        />
       </div>
     </div>
   );
